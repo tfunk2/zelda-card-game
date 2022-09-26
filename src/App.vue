0e307b9916d9089b8586eb8f7f10bb9f80d11c9d
tfunk2 <tyler.j.funk2@gmail.com>
2022-09-26 01:40:17 -0600

    Recreated repo with changes from old repo. - Fetch all creatures. - Began categorizing data. - Created new card component to start displaying creatures.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Zelda Card Game</h1>
+  <h3>Food Creatures:</h3>
+  <CreatureCard
+    v-for="creature in foodCreatures"
+    :key="creature.id"
+    :creature="creature"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "@vue/reactivity";
+import { onMounted, computed } from "@vue/runtime-core";
+import CreatureCard from "./components/CreatureCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CreatureCard,
+  },
+  setup() {
+    const allZeldaData = ref();
+
+    const fetchAndSetZeldaData = async () => {
+      await fetch("https://botw-compendium.herokuapp.com/api/v2/all")
+        .then((resp) => resp.json())
+        .then(({ data }) => (allZeldaData.value = data));
+    };
+
+    const allCreatures = computed(() => {
+      if (allZeldaData.value) {
+        return allZeldaData.value.creatures;
+      }
+    });
+
+    const foodCreatures = computed(() => {
+      if (allCreatures.value) {
+        return allCreatures.value.food;
+      }
+    });
+
+    /**
+     * This is working as intended and setting allZeldaData on mount
+     */
+    onMounted(() => {
+      fetchAndSetZeldaData();
+    });
+
+    return {
+      allCreatures,
+      allZeldaData,
+      foodCreatures,
+    };
+  },
+};
 </script>
 
 <style>
@@ -19,7 +60,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/CreatureCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <h5>Name</h5>
+  <p>{{ creature.name }}</p>
+</template>
+
+<script>
+export default {
+  name: "CreatureCard",
+  props: {
+    creature: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
